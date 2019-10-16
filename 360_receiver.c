ea0f1fa7d77eeb5dfabc75959ef51826fdd65a2d
Mangero <mangero.git@gmail.com>
2019-10-16 19:48:27 +0200

    Add support for ATmega48A/PA/88A/PA/168A/PA/328/P
     - Use defines for register manipulation in 360_receiver.c instead of
       hard-coded values.
    
     - Move defines to separate file 360_receiver.h and add defines for
       ATtinyX5 and ATmega48A/PA/88A/PA/168A/PA/328/P boards.

--- 360_receiver.c
+++ 360_receiver.c
@@ -1,7 +1,10 @@
 // Define device if not already defined
-#ifndef __AVR_ATtiny85__
-#define __AVR_ATtiny85__
-#endif
+//#ifndef __AVR_ATtiny85__
+//#define __AVR_ATtiny85__
+//#endif
+//#ifndef __AVR_ATmega328P__
+//#define __AVR_ATmega328P__
+//#endif
 
 #include <math.h>
 #include <inttypes.h>
@@ -10,25 +13,7 @@
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
-
-#define INT_REG GIMSK  // Register controlling interrupt behaviour.
-#define DIR_REG DDRB   // Register controlling the data direction of the pins.
-#define IN_REG  PINB   // Register for reading values from pins.
-#define OUT_REG PORTB  // Register for writing values to pins.
-
-#define button_pin PB2 // Pin connected to button (physical pin 7 on ATtiny, 5 on xbox module).
-#define data_pin   PB4 // Data line (physical pin 3 on ATtiny, 6 on xbox module).
-#define clock_pin  PB3 // Clock line (physical pin 2 on ATtiny, 7 on xbox module).
-
-// Calculate Timer1 prescaler value. Works for all CPU frequencies which are a multiple of 125 kHz.
-// With avr-gcc this is calculated at compile-time.
-#define PRESCALE_SELECT ((uint8_t) (log(F_CPU / 125000UL) / log(2) + 1))
-
-// Macros for reading and writing to individual pins.
-#define SET_PIN(reg, pin) (reg |= (1 << pin))
-#define CLR_PIN(reg, pin) (reg &= ~(1 << pin))
-#define READ_PIN(pin) ((IN_REG >> pin) & 0x01)
-#define WRITE_PIN(pin, value) (value ? SET_PIN(OUT_REG, pin) : CLR_PIN(OUT_REG, pin))
+#include "360_receiver.h"
 
 // Commands for controlling the xbox controller module.
 static const uint16_t leds_cmd = 0x0084;     // Initializes the LEDs, leaving the center LED lit.
@@ -43,26 +28,26 @@ static volatile uint32_t millis_value;               // Button press duration in
 void send_data(uint16_t cmd)
 {
 	// Prepare data pin for sending data by setting it as output.
-	SET_PIN(DIR_REG, data_pin);
+	SET_PIN(DIR_REG, DATA_PIN);
 
 	// Synchronize with the clock to ensure data is valid on positive clock flank.
-	while (READ_PIN(clock_pin) == 0) {}
+	while (READ_PIN(CLOCK_PIN) == 0) {}
 
 	// Send command to the xbox module.
 	for (int8_t i = 9; i >= 0; i--) {
 		// Wait for clock pin to go low.
-		while (READ_PIN(clock_pin) == 1) {}
+		while (READ_PIN(CLOCK_PIN) == 1) {}
 
 		// Output next bit in the command to data pin.
-		WRITE_PIN(data_pin, ((cmd >> i) & 0x0001));
+		WRITE_PIN(DATA_PIN, ((cmd >> i) & 0x0001));
 
 		// Wait for clock pin to go high.
-		while (READ_PIN(clock_pin) == 0) {}
+		while (READ_PIN(CLOCK_PIN) == 0) {}
 	}
 
 	// Switch data pin to high impedence mode.
-	CLR_PIN(DIR_REG, data_pin); // Set as input.
-	WRITE_PIN(data_pin, 0);     // Pull pin low.
+	CLR_PIN(DIR_REG, DATA_PIN); // Set as input.
+	WRITE_PIN(DATA_PIN, 0);     // Set pin low.
 }
 
 // Interrupt vector for INT0 (button press).
@@ -72,33 +57,34 @@ ISR(INT0_vect)
 	INT_REG &= ~(1 << INT0);
 }
 
-// Interrupt vector for Timer1, triggers every millisecond.
-ISR(TIMER1_COMPA_vect)
+// Interrupt vector for Timer, triggers every millisecond.
+ISR(TIMER_COMP_VECT)
 {
 	// Increment millisecond counter.
 	millis_value++;
 	// Update button status (used for debouncing).
-	button_status = (button_status << 1) | READ_PIN(button_pin);
+	button_status = (button_status << 1) | READ_PIN(BUTTON_PIN);
 }
 
 void timer_init()
 {
 	// Set Output Compare Register to match with timer counter every millisecond.
-	OCR1C = 125;
+	OCR_REG = 125;
 	// Enable Clear Timer on Compare Match.
-	TCCR1 = (1 << CTC1);
-	// Enable interrupt on match with Compare Register A.
-	TIMSK = (1 << OCIE1A);
-}
-
-void timer_start()
-{
-	TCCR1 = (TCCR1 & 0xF0) | PRESCALE_SELECT; // TODO Fungerar denna mask med Mega 328P?
+	TCCRA_REG = (1 << CTC_BIT);
+	// Enable interrupt on match with Compare Register.
+	TIMSK_REG = (1 << OCI_BIT);
 }
 
 void timer_stop()
 {
-	GTCCR |= (1 << PSR1);
+	GTCCR |= (1 << PSR_BIT);
+}
+
+void timer_start()
+{
+	timer_stop();
+	TCCRB_REG |= PRESCALE_SELECT;
 }
 
 void timer_reset()
@@ -114,7 +100,7 @@ void sleep()
 	millis_value = 0;
 	// Set button_status value to avoid premature button release condition.
 	button_status = 0xFFFFFFFE;
-	// Enable interrupt on low INT0 (button_pin).
+	// Enable interrupt on low INT0 (BUTTON_PIN).
 	INT_REG |= (1 << INT0);
 	sleep_enable();
 	// The instruction following sei() will always run before any interrupts are triggered.
@@ -123,9 +109,10 @@ void sleep()
 	sleep_disable();
 }
 
-int main(void) {
+int main(void)
+{
 	// Enable all internal pull-ups except for the data pin, which has an external pull-up.
-	OUT_REG = (0x3F & ~(1 << data_pin));
+	OUT_REG = (0x3F & ~(1 << DATA_PIN));
 
 	// Wait for xbox module to be ready for communitation.
 	_delay_ms(100);
---
+++ 360_receiver.h
@@ -0,0 +1,59 @@
+// 360_receiver.h
+#ifndef _360_RECEIVER_H_
+#define _360_RECEIVER_H_ 1
+
+// Board specific defines.
+#if defined (__AVR_ATtiny25__) || defined (__AVR_ATtiny45__) || defined (__AVR_ATtiny85__)
+	// Calculate Timer1 prescaler value. Works for all CPU frequencies which are a multiple of 125 kHz.
+	// With avr-gcc this is calculated at compile-time.
+	#define PRESCALE_SELECT ((uint8_t) (log(F_CPU / 125000UL) / log(2) + 1))
+	#define INT_REG GIMSK    // Register controlling interrupt behaviour.
+	#define TIMER_COMP_VECT TIMER1_COMPA_vect // Timer compare vector.
+
+	#define OCR_REG OCR1C    // Output compare register for hardware timer.
+	#define TCCRA_REG TCCR1  // Timer/Counter Control Register.
+	#define TCCRB_REG TCCR1  // Timer/Counter Control Register.
+	#define TIMSK_REG TIMSK  // Timer Interrupt Mask Register.
+	#define OCI_BIT OCIE1A   // Output Compare Interrupt bit.
+	#define CTC_BIT CTC1     // Clear Timer on Compare bit.
+	#define PSR_BIT PSR1     // Prescaler Reset Timer/Counter.
+
+	#define DIR_REG DDRB     // Register controlling the data direction of the pins.
+	#define IN_REG  PINB     // Register for reading values from pins.
+	#define OUT_REG PORTB    // Register for writing values to pins.
+
+	#define BUTTON_PIN PB2   // Pin connected to button (physical pin 7 on ATtiny, 5 on xbox module).
+	#define DATA_PIN   PB4   // Data line (physical pin 3 on ATtiny, 6 on xbox module).
+	#define CLOCK_PIN  PB3   // Clock line (physical pin 2 on ATtiny, 7 on xbox module).
+#elif defined (__AVR_ATmega48A__)  || defined (__AVR_ATmega48PA__)  || \
+      defined (__AVR_ATmega88A__)  || defined (__AVR_ATmega88PA__)  || \
+      defined (__AVR_ATmega168A__) || defined (__AVR_ATmega168PA__) || \
+      defined (__AVR_ATmega328__)  || defined (__AVR_ATmega328P__)
+	#define PRESCALE_SELECT 5 // TODO Gör dynamisk
+	#define INT_REG EIMSK    // Register controlling interrupt behaviour.
+	#define TIMER_COMP_VECT TIMER2_COMPB_vect // Timer compare vector.
+
+	#define OCR_REG OCR2A    // Output compare register for hardware timer.
+	#define TCCRA_REG TCCR2A // Timer/Counter Control Register A.
+	#define TCCRB_REG TCCR2B // Timer/Counter Control Register B.
+	#define TIMSK_REG TIMSK2 // Timer Interrupt Mask Register.
+	#define OCI_BIT OCIE2B   // Output Compare Interrupt bit.
+	#define CTC_BIT WGM21    // Clear Timer on Compare bit.
+	#define PSR_BIT PSRASY   // Prescaler Reset Timer/Counter.
+
+	#define DIR_REG DDRD     // Register controlling the data direction of the pins.
+	#define IN_REG  PIND     // Register for reading values from pins.
+	#define OUT_REG PORTD    // Register for writing values to pins.
+
+	#define BUTTON_PIN PD2   // Pin connected to button.
+	#define DATA_PIN   PD4   // Data line.
+	#define CLOCK_PIN  PD3   // Clock line.
+#endif
+
+// Macros for reading and writing to individual pins.
+#define SET_PIN(reg, pin)     ((reg) |= (1 << (pin)))
+#define CLR_PIN(reg, pin)     ((reg) &= ~(1 << (pin)))
+#define READ_PIN(pin)         ((IN_REG >> (pin)) & 0x01)
+#define WRITE_PIN(pin, value) ((value) ? SET_PIN(OUT_REG, (pin)) : CLR_PIN(OUT_REG, (pin)))
+
+#endif // _360_RECEIVER_H_
